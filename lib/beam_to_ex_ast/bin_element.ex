77d09ec68eeee756cd8d67d348d103febd31a67c
Olafur Arason <olafura@olafura.com>
2019-12-09 15:42:20 -0500

    mix format

--- lib/beam_to_ex_ast/bin_element.ex
+++ lib/beam_to_ex_ast/bin_element.ex
@@ -17,12 +17,12 @@ defimplEx BeamToExAst.BinElement, {:bin_element, _ln, _block, _, _}, for: Transl
         %{parents: [:bin, :match | _]} = opts
       ) do
     opts = Map.update!(opts, :parents, &[:bin_element | &1])
-    {:::, [line: ln], [Translate.to_elixir({:var, ln2, v1}, opts), {type, [line: ln], nil}]}
+    {:"::", [line: ln], [Translate.to_elixir({:var, ln2, v1}, opts), {type, [line: ln], nil}]}
   end
 
   def to_elixir({:bin_element, _ln, block, {:integer, _ln2, i}, _}, opts) do
     opts = Map.update!(opts, :parents, &[:bin_element | &1])
-    {:::, [], [Translate.to_elixir(block, opts), i]}
+    {:"::", [], [Translate.to_elixir(block, opts), i]}
   end
 
   def to_elixir({:bin_element, _ln, block, _, _}, opts) do
