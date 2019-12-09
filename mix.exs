824646e7e58b39511612bf05b54d9454a27e936f
Olafur Arason <olafura@olafura.com>
2019-12-09 15:44:07 -0500

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BeamToExAst.Mixfile do
   def project do
     [
       app: :beam_to_ex_ast,
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.1",
       description: "Beam AST to Elixir AST transpiler",
       package: package(),
